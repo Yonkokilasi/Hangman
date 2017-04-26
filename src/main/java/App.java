80647f7a9aa209a353c2f5cb6840e66bfded3014
Yonkokilasi <yonkokilasi@gmail.com>
2017-04-26 19:09:27 +0300

    Commented code for others to understand

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,13 +4,14 @@ public class App {
     public static void main(String[] args) {
       Console myConsole = System.console();
       StringBuilder mystery = new StringBuilder();
-      int MAX_GUESSES = 7;
-      int incorrectGuesses = 0;
-      boolean continueGame = true;
+      int MAX_GUESSES = 7; //   These are the maximum guesses a user has
+      int incorrectGuesses = 0; // These are the number of incorrectGuesses
+      boolean continueGame = true; // This boolean determines if the game will continue or stop
 
       System.out.println("Enter a word to begin");
     String challenge = myConsole.readLine();
     challenge = challenge.toLowerCase();
+    // The loop below gets the length of the input and replaces each character with a dash so that the next user will just see dashes .
     for ( int i= 0 ; i < challenge.length()*2 ; i ++ ) {
         if (i % 2 == 0 ) {
              mystery.append("_");
@@ -18,6 +19,9 @@ public class App {
             mystery.append(" ");
         }
     }
+    // end of loop above
+
+    // The while loop below is in relation the the boolean above and will keep the game running until the number of incorrect guesses is reached or the user guesses the word.
     while (continueGame) {
     // String replaced = challenge.replace("H_ _ _o");
         System.out.println(mystery);
@@ -30,11 +34,14 @@ public class App {
                 isCorrect = true;
             }
         }
+        //Loop below determines if the user has won the game . it tajes mystery and converts it to a string from there it replaces the empty spaces with nothing thus bringing the word together and then compares it to challenge the initial input
         if (mystery.toString().replace(" ","").equals(challenge)){
-            System.out.println(mystery);
+            System.out.println("Your good the word entered was " + mystery);
             System.out.println("You won!");
             continueGame = false;
         }
+
+        //Checks if the guess is correct . if not correct it will increment the variable (incorrectGuesses)
         if(!isCorrect) {
             incorrectGuesses++;
         }
@@ -44,5 +51,6 @@ public class App {
             continueGame = false;
         }
     }
+    // End of while loop
 }
 }
